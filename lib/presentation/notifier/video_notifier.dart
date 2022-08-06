acc372fc3e5659e2ce137be88e784d0fe665649b
onishi taisei <taisei.developer@gmail.com>
2022-08-06 18:26:07 +0900

    create notifier class

---
+++ lib/presentation/notifier/video_notifier.dart
@@ -0,0 +1,26 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:youtubeapi/domain/model/video_model.dart';
+import 'package:youtubeapi/infrastructure/video_repository.dart';
+
+class VideoNotifier extends StateNotifier<AsyncValue<List<VideoModel>>> {
+  VideoNotifier(this._videoRepository) : super(const AsyncValue.loading()) {
+    searchVideo('flutter');
+  }
+
+  final VideoRepository _videoRepository;
+
+  Future<void> searchVideo(String word) async {
+    if (word.isEmpty) {
+      return;
+    }
+
+    state = const AsyncValue.loading();
+
+    try {
+      final videoList = await _videoRepository.fetchVideos(word);
+      state = AsyncValue.data(videoList);
+    } on Exception catch (error) {
+      state = AsyncValue.error(error);
+    }
+  }
+}
