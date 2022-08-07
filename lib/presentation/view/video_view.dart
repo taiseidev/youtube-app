9ce790adaa10c9b8973f875c860143257bd3b17e
onishi taisei <taisei.developer@gmail.com>
2022-08-07 18:31:13 +0900

    change title font in appbar

--- lib/presentation/view/video_view.dart
+++ lib/presentation/view/video_view.dart
@@ -3,6 +3,8 @@ import 'package:youtubeapi/presentation/view/components/video_app_bar.dart';
 import 'package:youtubeapi/presentation/view/components/video_body.dart';
 
 class VideoView extends StatelessWidget {
+  const VideoView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
